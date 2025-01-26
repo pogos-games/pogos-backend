edfc12b245444ee7ae5ae69ddf3c3096cb41d62f
point <maxime.point2@etu.uca.fr>
2025-01-26 14:23:13 +0100

    modif random generator

--- libs/tools/src/game/entities/game.entity.ts
+++ libs/tools/src/game/entities/game.entity.ts
@@ -4,6 +4,7 @@ import { Card } from 'apps/pogos-games/src/cards/model/card.interface';
 import { GameResponse } from '../dto/response/game-response.interface';
 import { GamePlayerResponse } from '../dto/response/game-player-response.interface';
 import { GameActionRequest } from '../dto/request/game-action-request.interface';
+import { randomInt } from 'crypto';
 
 export abstract class Player {
     id: string;
@@ -117,7 +118,7 @@ export abstract class Game<TResponse extends GameResponse,
 
     public shuffle<T>(array: T[]): T[] {
         for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+            const j = randomInt(0, i + 1); // Random index from 0 to i
             [array[i], array[j]] = [array[j], array[i]];  // Swap elements
         }
         return array;
