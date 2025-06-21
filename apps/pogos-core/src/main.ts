26c603173e6ab74ae142692ee1c300994af442a1
Baptiste D <bapt.14@hotmail.com>
2025-06-21 18:13:11 +0200

    ✨notifications in real-time with sockets

--- apps/pogos-core/src/main.ts
+++ apps/pogos-core/src/main.ts
@@ -4,25 +4,12 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
 declare const module: any;
-
-// Custom IoAdapter pour définir le path socket.io avec préfixe /api/games
-class CustomIoAdapter extends IoAdapter {
-  createIOServer(port: number, options?: any) {
-    return super.createIOServer(port, {
-      ...options,
-      path: '/api/games/socket.io',  // 👈 path important à synchroniser avec le front
-    });
-  }
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
-  app.useWebSocketAdapter(new CustomIoAdapter(app));
 
   const config = new DocumentBuilder()
     .setTitle('POGOS CORE')
@@ -47,7 +34,7 @@ async function bootstrap() {
     },
   });
 
-  app.useGlobalPipes(new ValidationPipe({transform:true}));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await app.listen(configService.get('CORE_PORT'));
 
