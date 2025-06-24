391d8df369780102bb004807c815f22284967d87
point <maxime.point2@etu.uca.fr>
2025-06-24 10:29:35 +0200

    :bug: fix action poker

--- apps/pogos-games/src/poker/dto/request/uno-start-request.class.ts
+++ apps/pogos-games/src/poker/dto/request/uno-start-request.class.ts
@@ -1,5 +1,5 @@
 import { GameStartRequest } from '../../../../../../libs/tools/src/game/dto/request/game-start-request.class';
-import { UnoCard } from '../../../new uno/entities/uno-card.interface';
+import { UnoCard } from '../../../uno/entities/uno-card.interface';
 
 export class UnoStartRequest extends GameStartRequest{
   deck?: UnoCard[]
